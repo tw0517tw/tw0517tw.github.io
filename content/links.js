d9b4fc557d8645360682cc3b45014b7ae97d61e1
tw0517tw <tw0517tw@gmail.com>
2018-10-24 23:36:11 +0800

    add blog link

--- content/links.js
+++ content/links.js
@@ -1,4 +1,5 @@
 module.exports = [
+  { name: 'blog', url: 'https://medium.com/@tw0517tw' },
   { name: 'facebook', url: 'https://www.facebook.com/tw0517tw' },
   { name: 'github', url: 'https://github.com/tw0517tw' },
   { name: 'plurk', url: 'https://www.plurk.com/tw0517tw' },
